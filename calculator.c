2d4f1dcc6661e47112e451d1fbe931d946fe56e1
HowardM09 <hmullins28@brophybroncos.org>
2025-03-04 14:12:18 -0700

    testing

--- calculator.c
+++ calculator.c
@@ -1,12 +1,10 @@
-#include <stdio.h>
 // Here is the calculator Code:
 // Please include: subtraction, multiplication, and division.
 // If you are confused with the code, do not be afraid to google
 // the functions, data types, the int main(void), format specifiers etc.
 
 int main(void) {
-  int x; // instead of indenting after every line, you must use ; to end the
-         // line
+  int x;
   int y;
 
   printf("Type your first number: ");
@@ -15,12 +13,6 @@ int main(void) {
   scanf("%i", &y);
 
   printf("\nThe first number is %i and the second number is %i", x, y);
-
-  int sum = x + y;
-  printf("\n\nThe sum of the numbers is %i", sum);
-  
-  int product = x * y;
-  printf("\n\nThe product of the numbers is %i", product);
 
   int difference = x - y;
   printf("\n\nThe difference of the numbers is %i", difference);
